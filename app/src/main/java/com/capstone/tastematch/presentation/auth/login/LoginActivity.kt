6aae9c3ad9ec21dc87c9d19b9d977322eb6483f5
andimulyafirman <andimulya1310@gmail.com>
2023-06-12 22:01:47 +0700

    implement authentication

--- app/src/main/java/com/capstone/tastematch/presentation/auth/login/LoginActivity.kt
+++ app/src/main/java/com/capstone/tastematch/presentation/auth/login/LoginActivity.kt
@@ -1,12 +1,62 @@
 package com.capstone.tastematch.presentation.auth.login
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.capstone.tastematch.R
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.capstone.tastematch.databinding.ActivityLoginBinding
+import com.capstone.tastematch.presentation.auth.register.RegisterActivity
+import com.capstone.tastematch.presentation.main.MainActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.tvRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnLogin.setOnClickListener {
+            val email = binding.edtEmail.text.toString()
+            val pass = binding.edtPassword.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+
+
+//  masih terjadi error ketika regsiter di lakukan maka akan menuju ke halaman main, di karenakan code di bawah ini,
+//    kode ini berguna untuk memastikan tidak masuk ke halaman login jika sudah pernah login sebelumnya
+    override fun onStart() {
+        super.onStart()
+
+        if(firebaseAuth.currentUser != null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
